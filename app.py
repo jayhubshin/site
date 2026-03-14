46d6d9e0d3d588ebe1cd7e0a1b0c7f5f64219e50
jayhubshin <75716637+jayhubshin@users.noreply.github.com>
2026-03-15 02:29:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -129,11 +129,11 @@ try:
                             get_position='[lon, lat]',
                             get_text='count_text',
                             get_color=[255, 255, 255],
-                            get_size=45,                 # 기준 크기
+                            get_size=40,                 # 기준 크기
                             size_units="'meters'",       # 화면 픽셀 기준
                             # --- 줌 대응 핵심 설정 ---
                             size_min_pixels=3,          # 지도를 멀리서 봐도 최소 12px 유지
-                            size_max_pixels=45,          # 지도를 아주 가까이서 봐도 최대 30px 제한
+                            size_max_pixels=40,          # 지도를 아주 가까이서 봐도 최대 30px 제한
                             # -----------------------
                             get_alignment_baseline="'center'",
                             get_text_anchor="'middle'",
